1a850f911ec88ed38ee7a861021a43141044f10d
Bchoque2005 <brian.choque@uab.edu.bo>
2025-06-05 17:54:23 -0400

    actualizado

---
+++ test/test_calculator.py
@@ -0,0 +1,18 @@
+import unittest
+
+from src.calculator import suma, resta, multiplicacion, division
+
+class CalculatorTest(unittest.TestCase):
+   
+    def test_sum(self):
+        self.assertEqual(suma(2, 3), 5)
+
+    def test_resta(self):
+        self.assertEqual(resta(10, 5), 5)
+
+    def test_multiplicacion(self):
+        self.assertEqual(multiplicacion(4, 5), 20)
+
+    def test_division(self):
+        self.assertEqual(division(10, 2), 5)
+        
